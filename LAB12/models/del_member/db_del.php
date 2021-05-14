4e422cbb478f600fc359f3a51c831b87417ea511
phatcharaporn0500 <620112418112@bru.ac.th>
2021-05-14 11:06:09 +0700

    LAB12

---
+++ LAB12/models/del_member/db_del.php
@@ -0,0 +1,17 @@
+<?php
+require "../conDB.php";
+
+$id = $_GET['id'];
+
+try{
+
+
+ $SQL = "DELETE FROM tb_member
+         WHERE member_id = '$id'
+         ";
+$conn->query($SQL);
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+?>
